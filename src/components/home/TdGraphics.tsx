dfd3ad8d205171b0954889a118e91cbb466c774c
moaztobok <moaztobok@gmail.com>
2024-08-14 19:29:10 +0100

    fixing hero assets

--- src/components/home/TdGraphics.tsx
+++ src/components/home/TdGraphics.tsx
@@ -1,18 +1,40 @@
 'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useMediaQuery } from 'usehooks-ts'
 
 const TdGraphics = () => {
+  const [isClient, setIsClient] = useState(false)
   const matches = useMediaQuery('(min-width: 768px)')
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  if (!isClient) {
+    return null // or a loading indicator, or a default view
+  }
 
   return (
     <>
-      {matches && <iframe src='https://my.spline.design/retrofuturismbganimation-05267393b9e06aacc4c5fed930104250/' className='absolute h-full -z-1' width='120%' height='120%'></iframe>
-      }
-      <Image src='/images/background.jpg' layout='fill' objectFit='cover' alt='bg-graphics' className='absolute h-full w-full' />
+      {matches ? (
+        <iframe
+          src='https://my.spline.design/retrofuturismbganimation-05267393b9e06aacc4c5fed930104250/'
+          className='absolute h-full -z-1'
+          width='120%'
+          height='120%'
+        ></iframe>
+      ) : (
+        <Image
+          width={1920}
+          height={1080}
+          src='/images/background.jpg'
+          objectFit='cover'
+          alt='bg-graphics'
+          className='absolute h-full w-full object-cover'
+        />
+      )}
     </>
-
   )
 }
 
