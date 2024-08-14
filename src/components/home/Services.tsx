615bee4db94a8239d70f703e2b3b95aa18934fd3
moaztobok <moaztobok@gmail.com>
2024-08-14 16:10:51 +0100

    fade-in text

--- src/components/home/Services.tsx
+++ src/components/home/Services.tsx
@@ -4,11 +4,11 @@ import { CustomButton } from '../shared/Custom-btn'
 
 const Services = () => {
     return (
-        <div className='w-full h-screen flex items-center  bg-primary text-background '>
+        <div className='w-full min-h-[80vh] flex items-center  bg-primary text-background '>
             <div className='wrapper gap-4 flex flex-col'>
-                <h2 className='text-3xl font-semibold'>How we can help you</h2>
+                <h2 className='text-4xl font-semibold'>How we can help you</h2>
                 <ServicesAccordion />
-                <CustomButton label='View all services' className='text-lg bg-white text-primary w-fit mt-8' />
+                <CustomButton label='View all services' className='text-lg bg-white text-primary w-fit mt-8 hover:bg-white/90 font-semibold' />
             </div>
         </div>
     )
