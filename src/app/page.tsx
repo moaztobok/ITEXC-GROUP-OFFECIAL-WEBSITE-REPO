60d9c5631354347bded9dd7a0893df27dd10c1a5
moaztobok <moaztobok@gmail.com>
2024-08-20 16:42:37 +0100

    fixed imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import ItexcAcaddmy from "@/components/home/ItexcAcaddmy";
 import Customers from "@/components/home/Customers";
 import Engineering from "@/components/home/Engineering";
 import HeroSection from "@/components/home/HeroSection";
+import ItexcAcaddmy from "@/components/home/ItexcAcaddmy";
+import Process from "@/components/home/Process";
 import Projects from "@/components/home/Projects";
 import Services from "@/components/home/Services";
-import PlaceHolder from "@/components/shared/PlaceHolder";
-import Process from "@/components/home/Process";
 
 export default function Home() {
   return (
