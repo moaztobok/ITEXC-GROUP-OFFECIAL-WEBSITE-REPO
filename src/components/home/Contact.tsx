3ae7a75b3c6bb278de331339d11d637c16e516ff
moaztobok <moaztobok@gmail.com>
2024-08-20 16:45:25 +0100

    change color of services section

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -1,67 +1,71 @@
-import React from 'react'
-import GradualSpacing from '../magicui/gradual-spacing'
-import { Facebook, Hotel, Instagram, Linkedin, Mail, Phone } from 'lucide-react'
-import ContactForm from './Components/ContactForm'
-import { FadeElement } from '../magicui/fade-text'
+import React from "react";
+import GradualSpacing from "../magicui/gradual-spacing";
+import {
+  Facebook,
+  Hotel,
+  Instagram,
+  Linkedin,
+  Mail,
+  Phone
+} from "lucide-react";
+import ContactForm from "./Components/ContactForm";
+import { FadeElement } from "../magicui/fade-text";
 
 const Contact = () => {
-    return (
-        <section className="bg-muted relative overflow-hidden">
-            <FadeElement>
-                <div className=" flex wrapper flex-col md:flex-row  gap-8">
-                    <div className="flex flex-col justify-center md:flex-[0.5] gap-6">
-                        <span className="font-semibold ">Contact us</span>
-                        <GradualSpacing
-                            text="Let's get in touch!"
-                            className="justify-start  Title"
-                        />
-                        <p>
-                            Got questions about the Itexc group and it’s units feel free to reach out.
-                        </p>
-                        <div className='flex flex-col mt-10 gap-4'>
-                            <div className='flex text-xl  gap-4'>
-                                <Hotel size={24} className='mb-auto mt-1' />
-                                <p>
-                                    Subdivision of the commune, Lot N01, Building N02, Dar El-Beida, Algiers.
-                                </p>
-                            </div>
-                            <div className='flex text-xl  gap-4'>
-                                <Phone size={24} className='mb-auto mt-1' />
-                                <p>
-                                    +213 345 6789
-                                </p>
-                            </div>
-                            <div className='flex text-xl  gap-4'>
-                                <Mail size={24} className='mb-auto mt-1' />
-                                <p>
-                                    contact@itexcgroup.com
-                                </p>
-                            </div>
-                        </div>
+  return (
+    <section className="bg-muted relative overflow-hidden">
+      <FadeElement>
+        <div className=" flex wrapper flex-col md:flex-row  gap-8">
+          <div className="flex flex-col justify-center md:flex-[0.5] gap-6">
+            <span className="font-semibold ">Contact us</span>
+            <GradualSpacing
+              text="Let's get in touch!"
+              className="justify-start  Title"
+            />
+            <p>
+              Got questions about the Itexc group and it’s units feel free to
+              reach out.
+            </p>
+            <div className="flex flex-col mt-10 gap-4">
+              <div className="flex text-xl  gap-4">
+                <Hotel size={24} className="mb-auto mt-1" />
+                <p>
+                  Subdivision of the commune, Lot N01, Building N02, Dar
+                  El-Beida, Algiers.
+                </p>
+              </div>
+              <div className="flex text-xl  gap-4">
+                <Phone size={24} className="mb-auto mt-1" />
+                <p>+213 345 6789</p>
+              </div>
+              <div className="flex text-xl  gap-4">
+                <Mail size={24} className="mb-auto mt-1" />
+                <p>contact@itexcgroup.com</p>
+              </div>
+            </div>
 
-                        <div className='flex flex-col gap-4 mt-10'>
-                            <p className='text-2xl font-bold'>Follow Us</p>
-                            <span className='flex gap-4'>
-                                <span className='text-background bg-foreground p-2'>
-                                    <Instagram size={24} className='stroke-background' />
-                                </span>
-                                <span className='text-background bg-foreground p-2'>
-                                    <Facebook size={24} className='fill-background' stroke='0' />
-                                </span>
-                                <span className='text-background bg-foreground p-2'>
-                                    <Linkedin size={24} className='fill-background' stroke='1' />
-                                </span>
-                            </span>
-                        </div>
-                    </div>
-                    <div className="flex flex-col items-center justify-center md:flex-[0.5] gap-6">
-                        <ContactForm />
-                    </div>
-                </div>
-            </FadeElement>
+            <div className="flex flex-col gap-4 mt-10">
+              <p className="text-2xl font-bold">Follow Us</p>
+              <span className="flex gap-4">
+                <span className="text-background bg-foreground p-2">
+                  <Instagram size={24} className="stroke-background" />
+                </span>
+                <span className="text-background bg-foreground p-2">
+                  <Facebook size={24} className="fill-background" stroke="0" />
+                </span>
+                <span className="text-background bg-foreground p-2">
+                  <Linkedin size={24} className="fill-background" stroke="1" />
+                </span>
+              </span>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center md:flex-[0.5] gap-6">
+            <ContactForm />
+          </div>
+        </div>
+      </FadeElement>
+    </section>
+  );
+};
 
-        </section>
-    )
-}
-
-export default Contact
+export default Contact;
--- src/components/home/SubFooter.tsx
+++ src/components/home/SubFooter.tsx
@@ -1,47 +1,64 @@
-import { FadeElement } from '@/components/magicui/fade-text'
-import { Icon } from '@/components/navigation/icon'
-import { CustomButton } from '@/components/shared/Custom-btn'
-import { Input } from '@/components/ui/input'
-import Link from 'next/link'
-import React from 'react'
+import { FadeElement } from "@/components/magicui/fade-text";
+import { Icon } from "@/components/navigation/icon";
+import { CustomButton } from "@/components/shared/Custom-btn";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import React from "react";
 
 const SubFooter = () => {
-    return (
-        <section className='bg-foreground text-background'>
-            <FadeElement>
-                <div className='flex-col divide-y gap-4 my-0 wrapper'>
-                    <div className='flex flex-col-reverse md:flex-row w-full gap-16 justify-between py-10'>
-                        <div className='flex flex-col gap-8 justify-between '>
-                            <Icon icon='#1F00FF' wordmark='#FFFFFF' />
-                            <div className='grid grid-cols-2 md:flex justify-between flex-wrap w-full gap-8'>
-                                <Link href='/jobs' >Get a job</Link>
-                                <Link href='/partners' className='ms-auto' >Our partners</Link>
-                                <Link href='/products' >Products</Link>
-                                <Link href='/services' className='ms-auto' >Services</Link>
-                            </div>
-                        </div>
-                        <div className='flex flex-col gap-4 w-full md:max-w-[50%]'>
-                            <p>Join our newsletter</p>
-                            <div className='flex flex-col md:flex-row gap-8'>
-                                <Input placeholder='Enter your email' className='h-full text-lg min-h-12 rounded-none bg-transparent text-background placeholder:text-background' /> <CustomButton label='Subscribe' />
-                            </div>
-                            <p>By subscribing you agree to with our <Link href='/policies' className='underline'>Privacy Policy</Link></p>
-                        </div>
-                    </div>
-                    <div className='flex flex-col md:flex-row justify-between flex-wrap w-full gap-16   py-10'>
-                        <div className='flex justify-between flex-wrap gap-8'>
-                            <Link href='/policies' className='underline'>Privacy Policy</Link>
-                            <Link href='/terms-of-services' className='underline'>Terms of Service</Link>
-                            <Link href='/cookies' className='underline'>Cookies Settings</Link>
-                        </div>
-                        <p>
-                            © 2024 ITEXC GROUP. All rights reserved
-                        </p>
-                    </div>
-                </div>
-            </FadeElement>
-        </section>
-    )
-}
+  return (
+    <section className="bg-foreground text-background">
+      <FadeElement>
+        <div className="flex-col divide-y gap-4 my-0 wrapper">
+          <div className="flex flex-col-reverse md:flex-row w-full gap-16 justify-between py-10">
+            <div className="flex flex-col gap-8 justify-between ">
+              <Icon icon="#1F00FF" wordmark="#FFFFFF" />
+              <div className="grid grid-cols-2 md:flex justify-between flex-wrap w-full gap-8">
+                <Link href="/jobs">Get a job</Link>
+                <Link href="/partners" className="ms-auto">
+                  Our partners
+                </Link>
+                <Link href="/products">Products</Link>
+                <Link href="/services" className="ms-auto">
+                  Services
+                </Link>
+              </div>
+            </div>
+            <div className="flex flex-col gap-4 w-full md:max-w-[50%]">
+              <p>Join our newsletter</p>
+              <div className="flex flex-col md:flex-row gap-8">
+                <Input
+                  placeholder="Enter your email"
+                  className="h-full text-lg min-h-12 rounded-none bg-transparent text-background placeholder:text-background"
+                />{" "}
+                <CustomButton label="Subscribe" />
+              </div>
+              <p>
+                By subscribing you agree to with our{" "}
+                <Link href="/policies" className="underline">
+                  Privacy Policy
+                </Link>
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row justify-between flex-wrap w-full gap-16   py-10">
+            <div className="flex justify-between flex-wrap gap-8">
+              <Link href="/policies" className="underline">
+                Privacy Policy
+              </Link>
+              <Link href="/terms-of-services" className="underline">
+                Terms of Service
+              </Link>
+              <Link href="/cookies" className="underline">
+                Cookies Settings
+              </Link>
+            </div>
+            <p>© 2024 ITEXC GROUP. All rights reserved</p>
+          </div>
+        </div>
+      </FadeElement>
+    </section>
+  );
+};
 
-export default SubFooter
+export default SubFooter;
