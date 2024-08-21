aba7559c4f0c74fea5e24ba07c217063c529ce94
moaztobok <moaztobok@gmail.com>
2024-08-21 15:14:09 +0100

    fixing imports

--- src/components/navigation/NavDropDown.tsx
+++ src/components/navigation/NavDropDown.tsx
@@ -1,30 +1,30 @@
 import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 import {
-    NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuTrigger
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuTrigger
 } from "../ui/navigation-menu";
 
 const NavDropDown = ({
-    title,
-    children,
-    className
+  title,
+  children,
+  className
 }: {
-    title: string;
-    children: ReactNode;
-    className?: string;
+  title: string;
+  children: ReactNode;
+  className?: string;
 }) => {
-    return (
-        <NavigationMenuItem className="bg-transparent">
-            <NavigationMenuTrigger className="hover:opacity-85 focus:text-background bg-transparent hover:text-background focus:bg-transparent data-[active]:bg-transparent data-[state=open]:bg-transparent rounded-none">
-                {title}
-            </NavigationMenuTrigger>
-            <NavigationMenuContent>
-                <ul className={cn(className)}>{children}</ul>
-            </NavigationMenuContent>
-        </NavigationMenuItem>
-    );
+  return (
+    <NavigationMenuItem className="bg-transparent">
+      <NavigationMenuTrigger className="hover:opacity-85 focus:text-background bg-transparent hover:text-background focus:bg-transparent data-[active]:bg-transparent data-[state=open]:bg-transparent rounded-none">
+        {title}
+      </NavigationMenuTrigger>
+      <NavigationMenuContent>
+        <ul className={cn(className)}>{children}</ul>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+  );
 };
 
 export default NavDropDown;
