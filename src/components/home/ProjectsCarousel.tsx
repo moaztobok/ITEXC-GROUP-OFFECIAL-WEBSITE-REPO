185b5631e8ef0da6eac5d52e6f1221fc47a957c1
moaztobok <moaztobok@gmail.com>
2024-09-30 17:35:34 +0100

    mobile carousel indicators added

--- src/components/home/ProjectsCarousel.tsx
+++ src/components/home/ProjectsCarousel.tsx
@@ -2,30 +2,38 @@
 import { CardContent } from "@/components/ui/card";
 import {
   Carousel,
+  CarouselApi,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious
 } from "@/components/ui/carousel";
 import { CarouselData } from "@/data/types";
-import { truncateString } from "@/lib/utils";
+import { cn, truncateString } from "@/lib/utils";
+import { Dot } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 const ProjectsCarousel = ({ data }: { data: CarouselData[] }) => {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
   return (
-    <Carousel className="max-w-[100vw] md:mt-10">
-      <CarouselContent className="gap-6 max-w-[100vw] md:mx-52 lg:mx-96 my-auto ">
-        {data.map((project) => (
-          <CarouselItem key={project.id} className="md:max-w-xl pl-7">
-            <CardContent className="flex my-auto flex-col p-0 gap-4">
-              <Image
-                src={project.image}
-                width={400}
-                height={300}
-                className="w-full cursor-pointer h-80  hover:scale-105 transition-all duration-300 object-cover"
-                alt={project.title}
-              />
+    <Carousel setApi={setApi} className="max-w-[100vw] md:mt-10 relative">
+      <CarouselContent className="gap-6 md:mx-52 lg:mx-96 my-auto relative">
+        {data.map((project, index) => (
+          <CarouselItem key={index} className="md:max-w-xl  max-w-[100vw]">
+            <CardContent className="flex my-auto flex-col p-4 md:p-6  gap-4 w-screen md:w-auto">
+              <div className="relative">
+                <Image
+                  src={project.image}
+                  width={400}
+                  height={300}
+                  className="w-full cursor-pointer aspect-[16/10] hover:scale-105 transition-all duration-300 object-cover"
+                  alt={project.title}
+                />
+                <div className="absolute bottom-2 right-2 flex md:hidden"></div>
+              </div>
               <Link href={project.url} className="text-lg font-semibold">
                 {project.title}
               </Link>
@@ -36,8 +44,24 @@ const ProjectsCarousel = ({ data }: { data: CarouselData[] }) => {
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious className="top-1/3 hidden md:flex bg-foreground start-[15%] w-16 h-16 text-4xl text-background border-none  transition-all duration-200" />
-      <CarouselNext className="top-1/3 hidden md:flex bg-foreground end-[15%] w-16 h-16 text-4xl  text-background border-none transition-all duration-200" />
+      <div className="my-4 bottom-0 flex md:hidden z-50">
+        {data.map((_, dotIndex) => (
+          <Dot
+            size={42}
+            strokeWidth={2}
+            key={dotIndex}
+            className={cn(`cursor-pointer md:hidden opacity-35`, {
+              "opacity-100 text-primary": current === dotIndex
+            })}
+            onClick={() => {
+              api?.scrollTo(dotIndex);
+              setCurrent(dotIndex);
+            }}
+          />
+        ))}
+      </div>
+      <CarouselPrevious className="top-1/3 hidden md:flex bg-foreground start-[15%] w-16 h-16 text-4xl text-background border-none transition-all duration-200" />
+      <CarouselNext className="top-1/3 hidden md:flex bg-foreground end-[15%] w-16 h-16 text-4xl text-background border-none transition-all duration-200" />
     </Carousel>
   );
 };
