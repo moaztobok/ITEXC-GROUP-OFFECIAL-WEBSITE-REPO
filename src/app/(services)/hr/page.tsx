6e6c3157e1c764f30105b4120b79558981246615
moaztobok <moaztobok@gmail.com>
2024-10-03 10:58:52 +0100

    fix build error

--- src/app/(services)/hr/page.tsx
+++ src/app/(services)/hr/page.tsx
@@ -1,12 +1,9 @@
-import React from "react";
 import HeroSection from "./_components/HeroSection";
-import SectionWrapper from "@/components/shared/SectionWrapper";
 
 const page = () => {
   return (
     <main className="min-h-screen">
       <HeroSection />
-      <SectionWrapper></SectionWrapper>
     </main>
   );
 };
