f3b8b8fdc6fdfa82ce2cd8abd99f26be90d17fb9
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-10-28 15:29:58 +0100

    add 2 new sections for units

--- src/app/units/dev/page.tsx
+++ src/app/units/dev/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import HeroSection from "./_components/HeroSection";
 import { Metadata } from "next";
+import Introduction from "../_components/Introduction";
+import Responsibilities from "../_components/Responsibilities";
 
 export const metadata: Metadata = {
   title: "ITEXC Engineering",
@@ -12,6 +14,28 @@ const page = () => {
   return (
     <main className="min-h-screen">
       <HeroSection />
+      <Introduction
+        title="Our Core Unit"
+        description="The Software Development Division is the core division of ITEXC Group,
+        encompassing all software development activities across various projects
+        and other divisions."
+      />
+      <Responsibilities
+        title="Responsibilities and Importance"
+        description="The Software Development Division is responsible for designing,
+            developing, and maintaining software solutions for ITEXC
+            Group's clients and internal projects. As the core division, it
+            plays a crucial role in driving innovation and delivering
+            high-quality software products that meet client needs and industry
+            standards across all of ITEXC Group's services. This
+            division's work spans from web and mobile applications to
+            complex enterprise systems, ensuring that ITEXC Group remains at the
+            forefront of technological advancements. The Software Development
+            Division collaborates closely with other divisions, providing
+            technical expertise and support for their respective projects and
+            initiatives."
+        image="https://images.unsplash.com/photo-1653549892798-c8596ad08ee6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGN1YmUlMjAzZHxlbnwwfDF8MHx8fDA%3D"
+      />
     </main>
   );
 };
--- src/app/units/erp/page.tsx
+++ src/app/units/erp/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import HeroSection from "./_components/HeroSection";
 import { Metadata } from "next";
+import Responsibilities from "../_components/Responsibilities";
+import Introduction from "../_components/Introduction";
 
 export const metadata: Metadata = {
   title: "ITEXC ERP Solutions",
@@ -12,6 +14,25 @@ const page = () => {
   return (
     <main className="min-h-screen">
       <HeroSection />
+      <Introduction
+        title="What is ITEXC ERP?"
+        description="ITEXC ERP is the specialized Enterprise Resource Planning division of
+        ITEXC Group, focusing on implementing and customizing ERP solutions for
+        businesses across various industries."
+      />
+      <Responsibilities
+        title="Responsibilities and Importance"
+        description="The ERP Integration division, known as ITEXC ERP, specializes in
+            implementing and customizing Enterprise Resource Planning systems
+            for ITEXC Group's clients. This division plays a crucial role in
+            helping organizations streamline their business processes, improve
+            efficiency, and gain better control over their operations through
+            integrated ERP solutions. ITEXC ERP's expertise spans various ERP
+            platforms, ensuring that clients receive tailored solutions that
+            best fit their unique business needs and drive digital
+            transformation."
+        image="https://images.unsplash.com/photo-1653549892798-c8596ad08ee6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGN1YmUlMjAzZHxlbnwwfDF8MHx8fDA%3D"
+      />
     </main>
   );
 };
